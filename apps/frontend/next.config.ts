7538172103c4848b2ed965109f495c1b7cac95f8
DoFlowDev <developer@doflow.com>
2025-12-18 13:14:37 +0100

    Fix UI

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: "standalone", // <--- FONDAMENTALE PER DOCKER
   typescript: {
     ignoreBuildErrors: true,
   },
