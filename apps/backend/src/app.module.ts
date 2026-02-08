ea7b9f51a29900875002ef27e30cfc3fd3450143
DoFlowDev <developer@doflow.com>
2026-02-08 18:34:10 +0100

    upgrade-delivery

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -79,7 +79,7 @@ import { RedisModule } from './redis/redis.module';
           type: 'postgres' as const,
           url,
           autoLoadEntities: true,
-          synchronize: true, // <--- MESSO A TRUE PER CREARE LA TABELLA DELIVERY_TASKS
+          synchronize: false,
         };
       },
     }),
